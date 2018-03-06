7945777a290c76368905376fcfa9baf5603c49a2
Ben Eblen <beneblen@Bens-iMac.gateway>
2018-03-06 22:34:54 +1030

    Setup seperate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+require('./config/config')
+
+
+
 const _ = require('lodash')
 const {ObjectID} = require('mongodb')
 let express = require('express')
@@ -8,7 +12,7 @@ let {Todo} = require('./models/todo')
 let {User} = require('./models/user')
 
 let app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
