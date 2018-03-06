6758f6bd345d3fd9431a4e7f5fa6b048d6c30fa2
Ben Eblen <beneblen@Bens-iMac.gateway>
2018-03-06 19:42:34 +1030

    Add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,22 @@
+const {ObjectID} = require('mongodb')
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo')
+const {User} = require('./../server/models/user')
+
+ // Todo.remove({}).then((result) => {
+ //   console.log(result);
+ // })
+
+
+// Todo.findOneAndRemove({})
+// Todo.findByIdAndRemove()
+
+Todo.findOneAndRemove({_id:'5a9e58666458a4ceada9be36'}).then((todo) => {
+  console.log(todo);
+})
+
+
+Todo.findByIdAndRemove('5a9e58666458a4ceada9be36').then((todo) => {
+  console.log(todo);
+});
