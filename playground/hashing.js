4c763c92373220d9ef65ed2ccc876f0a3b7dd109
Ben Eblen <beneblen@Bens-iMac.gateway>
2018-03-07 23:04:26 +1030

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,32 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+let password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  })
+})
+
+let hashedPassword = '$2a$10$ic.mOpKSVqYICIKwPNCbiOdaioQQGgZMbZ7unwnrNqRDfq6UumhOu'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+})
+
+// let data = {
+//   id: 10
+// }
 
 
-let data = {
-  id: 10
-}
-
-
-
-let token = jwt.sign(data, '123abc');
-console.log(token);
-
-let decoded = jwt.verify(token, '123abc')
-console.log(decoded);
+//
+// let token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// let decoded = jwt.verify(token, '123abc')
+// console.log(decoded);
 
 
 // let message = 'I am user number 3';
