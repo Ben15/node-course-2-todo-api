3d100e6fa47044bab6c623f8b589632717609e19
Ben Eblen <beneblen@Bens-iMac.gateway>
2018-03-08 23:12:36 +1030

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,20 @@ UserSchema.methods.generateAuthToken = function () {
   })
 };
 
+
+
+UserSchema.methods.removeToken = function (token) {
+  let user = this;
+
+  return user.update({
+    $pull: {
+      tokens:{token}
+    }
+  })
+
+}
+
+
 UserSchema.statics.findByToken = function (token) {
   let User = this;
   let decoded;
